4b441238bc0fa1a08034b26d7e43168b26c82d6b
Jan-Bendikt Jagusch <jan.jagusch@quantco.com>
2024-12-30 10:48:59 +0100

    Use npm install instead of yarn install.

--- recipe/build.sh
+++ recipe/build.sh
@@ -56,7 +56,7 @@ popd
   # Install all dependencies except @vscode/ripgrep
   mv package.json package.json.orig
   jq 'del(.dependencies."@vscode/ripgrep")' package.json.orig | jq 'del(.devDependencies."@vscode/telemetry-extractor")' > package.json
-  yarn install
+  npm install
   # Install @vscode/ripgrep without downloading the pre-built ripgrep.
   # This often runs into Github API ratelimits and we won't use the binary in this package anyways.
   yarn add --ignore-scripts "@vscode/ripgrep@${VSCODE_RIPGREP_VERSION}" "@vscode/telemetry-extractor@${VSCODE_TELEMETRY_VERSION}"
