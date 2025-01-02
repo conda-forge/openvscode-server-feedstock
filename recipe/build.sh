d817d930268e557965e1841bd24f47531567856f
Jan-Bendikt Jagusch <jan.jagusch@quantco.com>
2025-01-02 15:37:40 +0100

    Debug.

--- recipe/build.sh
+++ recipe/build.sh
@@ -33,9 +33,7 @@ pushd remote
   VSCODE_RIPGREP_VERSION=$(jq -r '.dependencies."@vscode/ripgrep"' package.json)
   # Install all dependencies except @vscode/ripgrep
   mv package.json package.json.orig
-  jq 'del(.dependencies."@vscode/ripgrep")' package.json.orig > package.json
-  # Temporarily remove tree-sitter
-  jq 'del(.dependencies."@vscode/tree-sitter-wasm")' package.json > package.json
+  jq 'del(.dependencies."@vscode/ripgrep")' package.json.orig | jq 'del(.dependencies."@vscode/tree-sitter-wasm")' > package.json
   npm install
   # Install @vscode/ripgrep without downloading the pre-built ripgrep.
   # This often runs into Github API ratelimits and we won't use the binary in this package anyways.
