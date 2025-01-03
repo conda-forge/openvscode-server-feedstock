fe0c5be3978d20187ee7813025224c20bd25b94d
Jan-Bendikt Jagusch <jan.jagusch@quantco.com>
2025-01-03 16:55:40 +0100

    Add --omit optional.

--- recipe/build.sh
+++ recipe/build.sh
@@ -34,7 +34,7 @@ pushd remote
   # Install all dependencies except @vscode/ripgrep
   mv package.json package.json.orig
   jq 'del(.dependencies."@vscode/ripgrep")' package.json.orig > package.json
-  npm install --verbose
+  npm install --omit=optional --verbose
   # Install @vscode/ripgrep without downloading the pre-built ripgrep.
   # This often runs into Github API ratelimits and we won't use the binary in this package anyways.
   npm add --ignore-scripts "@vscode/ripgrep@${VSCODE_RIPGREP_VERSION}"
